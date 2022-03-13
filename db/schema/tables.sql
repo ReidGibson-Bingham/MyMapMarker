c8e72cfc5d47c6ba03ce083ae0a5f665de40ac16
P4ulH1ll <jsr3b0rn@gmail.com>
2022-03-13 00:49:20 +0000

    commiting

--- db/schema/tables.sql
+++ db/schema/tables.sql
@@ -8,13 +8,19 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP
 );
 
 CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  latitude float,
+  longitude float,
+  description TEXT,
+  created_at TIMESTAMP,
+  favourite_map BOOLEAN
 );
 
 CREATE TABLE points (
@@ -22,6 +28,12 @@ CREATE TABLE points (
   title VARCHAR(255) NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE
+  -- image
+  -- description
+  latitude float,
+  longitude float,
+  created_at TIMESTAMP,
+  favourite_point BOOLEAN
 );
 
 
