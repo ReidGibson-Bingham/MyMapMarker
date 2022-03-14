d53ba4fd7da0c7e191b139c3ab59c91c669836e8
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-03-13 17:14:55 -0700

    add infowindow on mark show coordinates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,31 +9,39 @@ function initMap() {
     zoom: 13,
     center: richmond,
   });
+
+  // Create an info window to share between markers.
+  const infowindow = new google.maps.InfoWindow();
+
   // The marker, positioned at richmond
   const marker = new google.maps.Marker({
     position: richmond,
     map: map,
-    //title: "Hello World!",
-  });
-
-  // Create an info window to share between markers.
-  const infoWindow = new google.maps.InfoWindow({
-    content: `i am infoWindow`,
-  });
-
-  // Add a click listener for each marker, and set up the info window.
-  marker.addListener("click", () => {
-    infoWindow();
-    //infoWindow.setContent(`hello`);
-    //infoWindow.open(marker.getMap(), marker);
+    title: `Lat:${richmond.lat}, Lng:${richmond.lng}`,
   });
 
   //This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, "click", (event) => {
     addMarker(event.latLng, map);
   });
+
+  marker.addListener("click", () => {
+    infowindow.close();
+    infowindow.setContent(marker.title);
+    console.log("title is", marker.title);
+    infowindow.open(marker.getMap(), marker);
+  });
+  // Add a click listener for each marker, and set up the info window.
+  // marker.addListener("click", (event) => {
+  //   infowindow.open({
+  //     anchor: marker,
+  //     map,
+  //     shouldFocus: false,
+  //   });
+  // });
+
   //Add a marker at the center of the map.
-  addMarker(richmond, map);
+  //addMarker(richmond, map);
 }
 
 const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -46,5 +54,7 @@ function addMarker(location, map) {
     position: location,
     label: labels[labelIndex++ % labels.length],
     map: map,
+    //optimized: false,
+    title: `Lat:${location.lat}, Lng:${location.lng}`,
   });
 }
