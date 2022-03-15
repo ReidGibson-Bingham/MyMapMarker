195d66e54cf9d680fa9341461613412dc267111a
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-03-14 21:58:33 -0400

    const pool, const body-parser

--- server.js
+++ server.js
@@ -8,16 +8,24 @@ const express = require("express");
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const morgan = require("morgan");
-
+const bodyParser = require("body-parser");
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
+const pool = new Pool({
+  user: 'jaspersm1',
+  password: 'TenaCityUX',
+  host: 'localhost',
+  database: 'midterm'
+});
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+
 app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(expressLayouts);
