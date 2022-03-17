a70667998086529126acd7e344298de7c0d41161
JLMaynardDesign <JLMaynardDesign@gmail.com>
2022-03-16 20:42:54 -0400

    get user location and render that map with pin indicating user's current location

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,17 +1,32 @@
 // Client facing scripts here
 
 // Initialize and add the map
+/*
+if (navigator.geolocation) {
+  console.log('geolocation available');
+  navigator.geolocation.getCurrentPosition(function(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    document.getElementById('latitude').textContent = lat;
+    document.getElementById('longitude').textContent = lng;
+    console.log(position);
+  });
+}
+*/
+
 function initMap() {
+
   // The location of Richmoind 49.16186001171447, -123.13926987802597
   const richmond = { lat: 49.16186001171447, lng: -123.13926987802597 };
   // The map, centered at richmond
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
-    center: richmond,
+    center: richmond
   });
 
   // Create an info window to share between markers.
   const infowindow = new google.maps.InfoWindow();
+
 
   // The marker, positioned at richmond
   const marker = new google.maps.Marker({
@@ -19,6 +34,28 @@ function initMap() {
     map: map,
     title: `Lat:${richmond.lat}, Lng:${richmond.lng}`,
   });
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(p) {
+      const latLng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+      //console.log(p.coords.latitude);
+      //console.log(p.coords.longitude);
+      const lat = p.coords.latitude;
+      const lng = p.coords.longitude;
+     document.getElementById('latitude').textContent = lat;
+     document.getElementById('longitude').textContent = lng;
+      // Set the map center on user location
+      map.setCenter(latLng);
+      new google.maps.Marker({
+        position: latLng,
+        map,
+        title: "you are here",
+      });
+    });
+  } else {
+    alert('Geo Location feature is not supported in this browser.');
+  }
+
 
   //This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, "click", (event) => {
@@ -86,26 +123,26 @@ function addMarker(location, map) {
     "</div>" +
     '<h1 id="firstHeading" class="firstHeading">Intersting Point</h1>' +
     '<div id="bodyContent">' +
-    '<form id="point-form">'+
-          '<label id = "text-label" for="point-text">'+
-          "Description: "+
-          "</label>"+
-          '<textarea id="point-text" name="text">'+
-          "  descriptions goes here   "+
-          "</textarea>"+
-          '<footer class="button-container">'+
-            '<button id="button-save" type="submit">'+
-            "Save"+
-            "</button>"+
-            '<button id="button-edit" type="submit">'+
-            "edit"+
-            "</button>"+
-            '<button id="button-delete" type="submit">'+
-            "Delete"+
-            "</button>"+
-          "</footer>"+
-    "</form>"+
-    '<p id="time-stamp">'+
+    '<form id="point-form">' +
+    '<label id = "text-label" for="point-text">' +
+    "Description: " +
+    "</label>" +
+    '<textarea id="point-text" name="text">' +
+    "  descriptions goes here   " +
+    "</textarea>" +
+    '<footer class="button-container">' +
+    '<button id="button-save" type="submit">' +
+    "Save" +
+    "</button>" +
+    '<button id="button-edit" type="submit">' +
+    "edit" +
+    "</button>" +
+    '<button id="button-delete" type="submit">' +
+    "Delete" +
+    "</button>" +
+    "</footer>" +
+    "</form>" +
+    '<p id="time-stamp">' +
     "(last visited June 22, 2009).</p>" +
     "</div>" +
     "</div>";
@@ -124,4 +161,5 @@ function addMarker(location, map) {
       };
     })(newMarker, infowindow)
   );
+
 }
